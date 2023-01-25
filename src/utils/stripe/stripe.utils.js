0a830e8a20bc239f1209bbcd744f0f7dd1436c50
SamurajTomasz <t.piatek@onet.eu>
2023-01-25 19:09:39 +0100

    add payment method

--- src/utils/stripe/stripe.utils.js
+++ src/utils/stripe/stripe.utils.js
@@ -3,3 +3,5 @@ import { loadStripe } from "@stripe/stripe-js";
 export const stripePromise = loadStripe(
   "pk_test_51MU9F6J9o1ZTEtmnYhLX2nFa933heuxcneaVvYCAg7x1Z2Gp7oZOalxNvp1q4NPeIggHHOhYG7BPsfin2ogO7Ccs00UMdQSALO"
 );
+
+// "pk_test_51MU9F6J9o1ZTEtmnYhLX2nFa933heuxcneaVvYCAg7x1Z2Gp7oZOalxNvp1q4NPeIggHHOhYG7BPsfin2ogO7Ccs00UMdQSALO"
